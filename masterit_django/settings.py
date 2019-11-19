40cb7e8dc47d684bed39ac915b6ee340d3abe0a1
vbc221 <vbc221@nyu.edu>
2019-11-18 21:05:43 -0600

    everything works

--- masterit_django/settings.py
+++ masterit_django/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django_heroku 
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,5 +130,3 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
-
-django_heroku.settings(locals())

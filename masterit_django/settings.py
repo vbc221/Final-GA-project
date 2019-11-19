16619a05ce441fa69c16d9e96d41cec2dfb3f9be
vbc221 <vbc221@nyu.edu>
2019-11-18 20:52:58 -0600

    hope this works.

--- masterit_django/settings.py
+++ masterit_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '7&%qt78ecug96%-q63a4^y&%+(c_mc-z8zxb%%ghf6^j#k^m=('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [‘0.0.0.0’, ‘localhost’,'polar-chamber-21260.herokuapp.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -124,9 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 LOGIN_REDIRECT_URL = '/'
 
+django_heroku.settings(locals())
